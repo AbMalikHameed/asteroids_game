a9947833fbd00b31dbb41ea60ce816de81ba700f
AbMalikHameed <abmalik.hameed99@gmail.com>
2026-01-07 18:22:49 +0000

    Added clock and dt. Set to 60fps

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from logger import log_state
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
@@ -13,6 +16,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick() / 1000
    
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
