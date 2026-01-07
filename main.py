42bbbe251309ec8244f87ed26421c5be4c760b0b
AbMalikHameed <abmalik.hameed99@gmail.com>
2026-01-07 20:31:10 +0000

    Added sapceship class and created instance of spaceship in main

--- main.py
+++ main.py
@@ -2,19 +2,23 @@ import pygame
 from constants import SCREEN_HEIGHT
 from constants import SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick() / 1000
