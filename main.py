af1eeee8af1e498ed31d452ad6067655fbbd4953
AbMalikHameed <abmalik.hameed99@gmail.com>
2026-01-09 12:48:56 +0000

    Added collisions between asteroids and player ship

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame
 from constants import *
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+import sys
 
 def main():
     pygame.init()
@@ -36,6 +37,13 @@ def main():
         updatable.update(dt)
         for obj in drawable:
             obj.draw(screen)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+        
         pygame.display.flip()
         
    
